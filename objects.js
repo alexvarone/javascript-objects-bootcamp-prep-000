39dec64ac2319f73777ac6ce3fbdfa54312f8048
Alex Varone <alexvarone@ymail.com>
2018-07-20 03:53:15 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,7 @@
+var playlist = {artistName: "songTitle"};
+
+function updatePlaylist(playlist, artist, song) {
+  playlist['Slowdive'] = 'Alison'
+  playlist['My Bloody Valentine'] = 'Sometimes'
+  return playlist;
+}
